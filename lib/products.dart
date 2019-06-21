5eb8f003015c3cdbaa4facb0f5892763d9a1e1dc
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-06-21 11:11:09 +0530

    splittingUp the files

---
+++ lib/products.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> product;
+
+  Products(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: product
+          .map(
+            (element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
+                ),
+          )
+          .toList(),
+    );
+  }
+}
