539e6397ffa7d4906ab6e302d09beebffa7165ed
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-06-23 16:36:12 +0530

    navigation

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,18 +4,22 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Product Detail'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Container(padding: EdgeInsets.all(10.0) ,child:(Text('Details!')),),
+              RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('BACK'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          ),
+        ));
   }
 }
