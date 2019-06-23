00ebb6da89ea61078e639da086a6ca732129f6f5
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-06-23 16:15:41 +0530

    navigation

--- lib/products.dart
+++ lib/products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
   final List<String> products;
 
@@ -7,22 +9,47 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   }
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ProductPage(),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Container();
+    }
+    return productCards;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return ListView(
-      children: products
-          .map(
-            (element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text(element)
-                    ],
-                  ),
-                ),
-          )
-          .toList(),
-    );
+    return _buildProductList();
   }
 }
