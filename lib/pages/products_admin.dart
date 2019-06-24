2d216280c78f0b90cb5c7ade28eb97f98e7724f0
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-06-24 16:25:41 +0530

    side drawer and hamburger icon

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../product_manager.dart';
-import './products_admin.dart';
+import './products.dart';
 
-class ProductsPage extends StatelessWidget {
+class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,22 +14,24 @@ class ProductsPage extends StatelessWidget {
               title: Text('Choose'),
             ),
             ListTile(
-              title: Text('Manage Products'),
+              title: Text('All Products'),
               onTap: () {
                 Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
                         builder: (BuildContext context) =>
-                            ProductsAdminPage()));
+                            ProductsPage()));
               },
             )
           ],
         ),
       ),
       appBar: AppBar(
-        title: Text('EasyList'),
+        title: Text('Manage Products'),
       ),
-      body: ProductManager(),
+      body: Center(
+        child: Text('Manage your Products'),
+      ),
     );
   }
 }
